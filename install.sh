09fdd4e760ea97ca3d1f5f45a3707d9d5a26558b
Amy Slagle <slagle@ehatek.org>
2014-06-26 12:01:54 -0400

    Rename some install scripts.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . install/install_mongo.sh
 . install/girder_s3_restore.sh
-. install/girder_setup
-. install/install_dashbaord.sh
+. install/girder_setup.sh
 . install/install_grits_api.sh
-. install/apache_setup
+. install/install_dashboard.sh
+. install/apache_setup.sh

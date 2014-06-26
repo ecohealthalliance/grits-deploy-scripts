81698d4ea80cffab17e24e27232f3d2456988196
Amy Slagle <slagle@ehatek.org>
2014-06-26 14:48:43 -0400

    Use full path to install scripts.

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-. install/install_mongo.sh
-. install/girder_s3_restore.sh
-. install/girder_setup.sh
-. install/install_grits_api.sh
-. install/supervisord_setup.sh
-. install/install_dashboard.sh
-. install/apache_setup.sh
+. ~/grits-deploy-scripts/install/install_mongo.sh
+. ~/grits-deploy-scripts/install/girder_s3_restore.sh
+. ~/grits-deploy-scripts/install/girder_setup.sh
+. ~/grits-deploy-scripts/install/install_grits_api.sh
+. ~/grits-deploy-scripts/install/supervisord_setup.sh
+. ~/grits-deploy-scripts/install/install_dashboard.sh
+. ~/grits-deploy-scripts/install/apache_setup.sh

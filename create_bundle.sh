ae52e4969e9367c232d2a8bab29b5a2c1782b58e
Nathan Breit <nabreit@gmail.com>
2014-07-17 09:36:30 +0000

    Excluding version control info from bundle

--- create_bundle.sh
+++ create_bundle.sh
@@ -13,11 +13,11 @@ git pull
 cd ..
 cd ../..
 git clone git@github.com:ecohealthalliance/diagnostic-dashboard.git ; cd diagnostic-dashboard
-git checkout integration
+git checkout tags/bundle0.0.1
 git pull
 cd ..
 git clone git@github.com:ecohealthalliance/grits-api.git ; cd grits-api
-git checkout integration
+git checkout tags/bundle0.0.0
 git pull
 aws s3 cp --recursive s3://classifier-data .
 mv translations corpora
@@ -25,4 +25,4 @@ git clone git@github.com:ecohealthalliance/annie.git ; cd annie
 git checkout master
 git pull
 cd ../../..
-tar -pczf grits-bundle.tar.gz grits-bundle
+tar -pczf grits-bundle.tar.gz grits-bundle --exclude-vcs

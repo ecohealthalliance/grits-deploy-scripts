b0ed1e8cbe6034d70c4f66b55ba4835cb1b09632
Nathan Breit <nabreit@gmail.com>
2014-07-28 01:18:15 +0000

    fixing bug where supervisor is not configured without flower password

--- install.sh
+++ install.sh
@@ -9,14 +9,12 @@ sudo pip install awscli virtualenv
 # Configure supervisor daemons
 sudo apt-get install -y supervisor
 cd ~/grits-deploy-scripts/supervisord
-# Loop over supervisord config files
-# substitute in environment vars
-# then put them in the config folder
 if [ -n "$FLOWER_PASSWORD" ]; then
-    for f in *; do
-        cat $f | envsubst | sudo tee /etc/supervisor/conf.d/$f
-    done
+    cat flowerd.conf | envsubst | sudo tee /etc/supervisor/conf.d/flowerd.conf
 fi
+cat celeryd.conf | envsubst | sudo tee /etc/supervisor/conf.d/celeryd.conf
+cat girderd.conf | envsubst | sudo tee /etc/supervisor/conf.d/girderd.conf
+cat gritsapid.conf | envsubst | sudo tee /etc/supervisor/conf.d/gritsapid.conf
 cd ~
 sudo supervisorctl update
 
